9bc4f59b6d33ed358fa5367c041823619f27ae8c
macchiatoism <macchiato.me@gmail.com>
2014-07-10 20:38:23 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -12,4 +12,4 @@ plot(data2$Date_and_Time,data2$Sub_metering_1,type="l",ylab="Energy sub metering
 lines(data2$Date_and_Time,data2$Sub_metering_2,col="red");#add more graph
 lines(data2$Date_and_Time,data2$Sub_metering_3,col="blue");#add more graph
 legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=c(1,1,1),col=c("black","red","blue"))#add legend
-dev.off();
+dev.off();#close device
